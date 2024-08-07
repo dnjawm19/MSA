c9d975ed6391b14ba76f7c7005d9aecb4f2417c7
dnjawm19 <dnjawm19@naver.com>
2024-08-07 14:27:35 +0900

    feat: 헤더에 포트 출력 기능 구현

---
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/AddServerPortHeaderFilter.java
@@ -0,0 +1,27 @@
+package com.sparta.msa_exam.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+
+@Component
+public class AddServerPortHeaderFilter extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                URI uri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+                if (uri != null) {
+                    String port = String.valueOf(uri.getPort());
+                    exchange.getResponse().getHeaders().add("server-port", port);
+                }
+            }));
+        };
+    }
+}
